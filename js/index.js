c8a474a7e7154fa7dffb630be73793ac823d2730
johnprovazek <johnprovazek@gmail.com>
2022-05-10 11:24:17 -0700

    added ability to adjust line color and thickness

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ window.addEventListener('load', function(event){
     var style = truchettiles.getAttribute("data-style")
     var color_1 = truchettiles.getAttribute("data-color-1")
     var color_2 = truchettiles.getAttribute("data-color-2")
+    var color_3 = truchettiles.getAttribute("data-color-3")
+    var thickness = truchettiles.getAttribute("data-outline-thickness")
     var size = truchettiles.getAttribute("data-size")
-    tilesInit(style,color_1,color_2,size)
+    tilesInit(style,color_1,color_2,color_3,thickness,size)
 });
 
 window.addEventListener('resize', function(event){
@@ -21,35 +23,49 @@ function demoTileSelect(element){
     var style = truchettiles.getAttribute("data-style")
     var color_1 = truchettiles.getAttribute("data-color-1")
     var color_2 = truchettiles.getAttribute("data-color-2")
+    var color_3 = truchettiles.getAttribute("data-color-3")
+    var thickness = truchettiles.getAttribute("data-outline-thickness")
     var size = truchettiles.getAttribute("data-size")
-    tilesInit(style,color_1,color_2,size)
+    tilesInit(style,color_1,color_2,color_3,thickness,size)
 }
 
 function demoColorChange(element) {
     if(demoIsColor(element.value)){
         var truchettiles = document.getElementById("truchettiles")
-        var style = truchettiles.getAttribute("data-style")
         if(element.id == "colorinput1"){
             truchettiles.setAttribute("data-color-1", element.value);
         }
         else if(element.id == "colorinput2"){
             truchettiles.setAttribute("data-color-2", element.value);
         }
+        else if(element.id == "colorinput3"){
+            truchettiles.setAttribute("data-color-3", element.value);
+        }
+        var style = truchettiles.getAttribute("data-style")
         var color_1 = truchettiles.getAttribute("data-color-1")
         var color_2 = truchettiles.getAttribute("data-color-2")
+        var color_3 = truchettiles.getAttribute("data-color-3")
+        var thickness = truchettiles.getAttribute("data-outline-thickness")
         var size = truchettiles.getAttribute("data-size")
-        tilesInit(style,color_1,color_2,size)
+        tilesInit(style,color_1,color_2,color_3,thickness,size)
     }
 }
 
 function demoSizeChange(element) {
     var truchettiles = document.getElementById("truchettiles")
+    if(element.id == "outlinethicknessslider"){
+        truchettiles.setAttribute("data-outline-thickness", element.value);
+    }
+    else if(element.id == "sizeslider"){
+        truchettiles.setAttribute("data-size", element.value);
+    }
     var style = truchettiles.getAttribute("data-style")
     var color_1 = truchettiles.getAttribute("data-color-1")
     var color_2 = truchettiles.getAttribute("data-color-2")
-    truchettiles.setAttribute("data-size", element.value);
+    var color_3 = truchettiles.getAttribute("data-color-3")
+    var thickness = truchettiles.getAttribute("data-outline-thickness")
     var size = truchettiles.getAttribute("data-size")
-    tilesInit(style,color_1,color_2,size)
+    tilesInit(style,color_1,color_2,color_3,thickness,size)
 }
 
 function demoIsColor(strColor){
@@ -63,7 +79,9 @@ function demoCanvasCopy(){
     var style = truchettiles.getAttribute("data-style")
     var color_1 = truchettiles.getAttribute("data-color-1")
     var color_2 = truchettiles.getAttribute("data-color-2")
+    var color_3 = truchettiles.getAttribute("data-color-3")
+    var thickness = truchettiles.getAttribute("data-outline-thickness")
     var size = truchettiles.getAttribute("data-size")
-    var html_string = "<canvas id=\"truchettiles\" data-style=\"" + style + "\" data-color-1=\"" + color_1 + "\" data-color-2=\"" + color_2 + "\" data-size=\"" + size + "\"></canvas>"
+    var html_string = "<canvas id=\"truchettiles\" data-style=\"" + style + "\" data-color-1=\"" + color_1 + "\" data-color-2=\"" + color_2 + "\" data-color-3=\"" + color_3 + "\" data-outline-thickness=\"" + thickness + "\" data-size=\"" + size + "\"></canvas>"
     navigator.clipboard.writeText(html_string);
 }
