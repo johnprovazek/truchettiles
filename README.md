9c80c21e45bb7b390b1beb9c95c1f4f4896e02c7
johnprovazek <johnprovazek@gmail.com>
2024-07-17 19:55:39 -0700

    cleaning up project

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-This project is an implementation of [truchet tiles](https://en.wikipedia.org/wiki/Truchet_tiles) using the `<canvas>` element. The purpose of this project is to create a template for interactive truchet tile backgrounds that could be used in web projects. You can access a demo at [johnprovazek.com/truchettiles](https://www.johnprovazek.com/truchettiles/).
+This project implements an interactive [truchet tile](https://en.wikipedia.org/wiki/Truchet_tiles) background that is designed to be used in web projects. You can access a demo at [johnprovazek.com/truchettiles](https://www.johnprovazek.com/truchettiles/).
 
 Built using vanilla JavaScript utilizing the `<canvas>` element.
 
@@ -11,19 +11,31 @@ Built using vanilla JavaScript utilizing the `<canvas>` element.
 To utilize truchet tiles in your own project start by placing a `<canvas>` element with the id "truchet-tiles" within the body of your html file. There are six attributes that need to be added to the `<canvas>` element to properly setup the truchet tiles.
 
 - `data-style` is the style of tile you would like to use. This value should be either in the range 1-15 or styles could be combined with "&" as a delimiter.
-- `data-color-1` is the main color of the tile. This could be represented as "#FFFFFF", "red", or "rgb(100,200,100)" for example. 
-- `data-color-2` is the background color of the tile. This could be represented as "#FFFFFF", "red", or "rgb(100,200,100)" for example. 
-- `data-color-3` is the color of the shape outline. This could be represented as "#FFFFFF", "red", or "rgb(100,200,100)" for example. 
+- `data-color-1` is the main color of the tile. This could be represented as "#FFFFFF", "red", or "rgb(100,200,100)" for example.
+- `data-color-2` is the background color of the tile. This could be represented as "#FFFFFF", "red", or "rgb(100,200,100)" for example.
+- `data-color-3` is the color of the shape outline. This could be represented as "#FFFFFF", "red", or "rgb(100,200,100)" for example.
 - `data-outline-thickness` is the size of the outline thickness. This needs to be in the range 0-100. A "0" thickness would represent no outline.
-- `data-size` is the size of the tiles. This needs to be in the range 1-50
+- `data-size` is the size of the tiles. This needs to be in the range 1-50.
 
 Here's an example `<canvas>` element with the attributes filled out:
-```
-<canvas id="truchet-tiles" data-style="7" data-color-1="#C41E3A" data-color-2="#0C2340" data-color-3="#FEDB00" data-outline-thickness="50" data-size="12"></canvas>
-```
-You can use the [demo](https://www.johnprovazek.com/truchettiles/) to test out different configurations. In the demo there is a button that can be used to copy the configuration to your project.
 
-You will need to include the files [truchettiles.js](./js/truchettiles.js) and [truchettiles.css](./css/truchettiles.css) in your project. You may need to make some adjustments to get this to work with your project. This project is built assuming the body has a margin of 0 and the height and width are at 100vw and 100vh.
+```html
+<canvas
+  id="truchet-tiles"
+  data-style="7"
+  data-color-1="#C41E3A"
+  data-color-2="#0C2340"
+  data-color-3="#FEDB00"
+  data-outline-thickness="50"
+  data-size="12"
+></canvas>
+```
+
+You can use the [demo page](https://www.johnprovazek.com/truchettiles/) to test out different configurations. In the [demo page](https://www.johnprovazek.com/truchettiles/) there is a button that can be used to copy the demo configuration to your project.
+
+You will need to include the files [truchettiles.js](./js/truchettiles.js) and [truchettiles.css](./css/truchettiles.css) in your project.
+
+You may need to make some adjustments to get this to work with your project. This project is built assuming the body has a margin of 0, min-height of 100lvh, and min-width of 100lvw
 
 ## Usage
 
@@ -31,11 +43,13 @@ Interact with the `<canvas>` with your mouse to change the orientation of the ti
 
 ## Credits
 
-The majority of the truchet tile designs I used were taken from this [website](http://arearugscarpet.blogspot.com/2014/04/the-curse-of-truchets-tiles.html).
+The majority of the truchet tile designs were taken from this [website](http://arearugscarpet.blogspot.com/2014/04/the-curse-of-truchets-tiles.html).
 
 ## Bugs & Improvements
 
+- Add a color picker.
+- Fix demo issues when zooming in on browsers.
+- Restructure the demo grid and fix overflow issue.
 - Add an info option to the demo page to give user instructions.
-- Investigate and fix bug where occasionally tiles will be drawn on canvas with small noticable gaps on the tiles edge.
-- Consider offering a React and Angular component implementation.
-- Use a linter and a style guide.
+- Consider adding value of the slider in the slider thumb.
+- Investigate and fix bug where occasionally tiles will be drawn on canvas with small noticeable gaps on the tiles edge.
--- js/index.js
+++ js/index.js
@@ -1,204 +1,219 @@
+// Handles page setup.
+window.onload = () => {
+  document.getElementById("demo-grid-refresh").addEventListener("click", () => {
+    demoRandomize();
+  });
+  document.getElementById("demo-grid-tiles-wrapper").addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+  });
+  document.getElementById("demo-grid-tiles-wrapper").addEventListener("touchstart", (event) => {
+    event.preventDefault();
+    if (event.touches.length === 1) {
+      demoTileChange(event.touches[0].target, false);
+    } else if (event.touches.length > 1 && event.touches[0].target.classList.contains("demo-grid-tile-selected")) {
+      demoTileChange(event.touches[1].target, true);
+    }
+  });
+  document.querySelectorAll(".demo-grid-tile-image").forEach((gridItem) => {
+    gridItem.addEventListener("click", (event) => {
+      demoTileChange(gridItem, event.ctrlKey);
+    });
+  });
+  document.querySelectorAll(".demo-grid-color-input").forEach((colorInput) => {
+    colorInput.addEventListener("input", () => {
+      demoColorChange(colorInput);
+    });
+    colorInput.addEventListener("focusout", () => {
+      demoColorFocusOut(colorInput);
+    });
+  });
+  document.querySelectorAll(".demo-grid-slider").forEach((slider) => {
+    slider.addEventListener("change", () => {
+      demoSliderChange(slider);
+    });
+  });
+  document.getElementById("demo-grid-copy-button").addEventListener("click", () => {
+    demoCanvasCopy();
+  });
+};
+
 // Handles changing the demo page tile style value.
-function demoSetStyle(style, load){
-  // Adding and removing tile-selected class to highlight the tiles selected.
-  let tiles = document.getElementsByClassName('tile-selected');
+function demoSetStyle(style, load) {
+  // Adding and removing demo-grid-tile-selected class to highlight the tiles selected.
+  let tiles = document.getElementsByClassName("demo-grid-tile-selected");
   while (tiles.length) {
-    tiles[0].classList.remove('tile-selected');
+    tiles[0].classList.remove("demo-grid-tile-selected");
   }
-  let tileStyles = style.split('&');
-  for (let i = 0; i < tileStyles.length; i++) {
-    document.getElementById('tile-image-' + tileStyles[i]).classList.add('tile-selected');
-  }
-  document.getElementById('truchet-tiles').setAttribute('data-style', style);
+  let tileStyles = style.split("&");
+  tileStyles.forEach((tileStyle) => {
+    document.getElementById("demo-grid-tile-image-" + tileStyle).classList.add("demo-grid-tile-selected");
+  });
+  document.getElementById("truchet-tiles").setAttribute("data-style", style);
   tilesSetStyle(style, load);
 }
 
-// Handles changing the demo page tile color1 value.
-function demoSetColor1(color, load){
+// Handles changing the demo page tile color values.
+function demoSetColor(color, number, load) {
   // Modifying the color selection background and value to match the color selected.
-  let colorInput1Element = document.getElementById('color-input-1');
-  colorInput1Element.style.backgroundImage = 'radial-gradient(circle, #F1F3F4 50%, ' + color + ')';
-  colorInput1Element.style.borderColor = '#292F33';
-  colorInput1Element.value = color;
-  colorInput1Element.setAttribute('data-value', color);
-  document.getElementById('truchet-tiles').setAttribute('data-color-1', color);
-  tilesSetColor1(color, load);
-}
-
-// Handles changing the demo page tile color2 value.
-function demoSetColor2(color, load){
-  // Modifying the color selection background and value to match the color selected.
-  let colorInput2Element = document.getElementById('color-input-2');
-  colorInput2Element.style.backgroundImage = 'radial-gradient(circle, #F1F3F4 50%, ' + color + ')';
-  colorInput2Element.style.borderColor = '#292F33';
-  colorInput2Element.value = color;
-  colorInput2Element.setAttribute('data-value', color);
-  document.getElementById('truchet-tiles').setAttribute('data-color-2', color);
-  tilesSetColor2(color, load);
-}
-
-// Handles changing the demo page tile color3 value.
-function demoSetColor3(color, load){
-  // Modifying the color selection background and value to match the color selected.
-  let colorInput3Element = document.getElementById('color-input-3');
-  colorInput3Element.style.backgroundImage = 'radial-gradient(circle, #F1F3F4 50%, ' + color + ')';
-  colorInput3Element.style.borderColor = '#292F33';
-  colorInput3Element.value = color;
-  colorInput3Element.setAttribute('data-value', color);
-  document.getElementById('truchet-tiles').setAttribute('data-color-3', color);
-  tilesSetColor3(color, load);
+  let colorInputElement = document.getElementById("demo-grid-color-input-" + number);
+  colorInputElement.style.backgroundImage = "radial-gradient(circle, #F1F3F4 50%, " + color + ")";
+  colorInputElement.style.borderColor = "#292F33";
+  colorInputElement.value = color;
+  colorInputElement.setAttribute("data-value", color);
+  document.getElementById("truchet-tiles").setAttribute("data-color-" + number, color);
+  tilesSetColor(color, number, load);
 }
 
 // Handles changing the demo page tile thickness value.
-function demoSetThickness(thickness, load){
+function demoSetThickness(thickness, load) {
   // Setting the slider value.
-  document.getElementById('outline-thickness-slider').value = thickness;
-  document.getElementById('truchet-tiles').setAttribute('data-outline-thickness', thickness);
+  document.getElementById("demo-grid-outline-thickness-slider").value = thickness;
+  document.getElementById("truchet-tiles").setAttribute("data-outline-thickness", thickness);
   tilesSetThickness(thickness, load);
 }
 
 // Handles changing the demo page tile size value.
-function demoSetSize(size, load){
+function demoSetSize(size, load) {
   // Setting the slider value.
-  document.getElementById('size-slider').value = size;
-  document.getElementById('truchet-tiles').setAttribute('data-size', size);
+  document.getElementById("demo-grid-size-slider").value = size;
+  document.getElementById("truchet-tiles").setAttribute("data-size", size);
   tilesSetSize(size, load);
 }
 
 // Handles loading all tile values at once.
-function demoLoadAllTiles(tilesStyle,tilesColor1,tilesColor2,tilesColor3,tilesThickness,tilesSize){
+function demoLoadAllTiles(tilesStyle, tilesColor1, tilesColor2, tilesColor3, tilesThickness, tilesSize) {
   demoSetStyle(tilesStyle, false);
-  demoSetColor1(tilesColor1, false);
-  demoSetColor2(tilesColor2, false);
-  demoSetColor3(tilesColor3, false);
+  demoSetColor(tilesColor1, 1, false);
+  demoSetColor(tilesColor2, 2, false);
+  demoSetColor(tilesColor3, 3, false);
   demoSetThickness(tilesThickness, false);
   demoSetSize(tilesSize, false);
   tilesLoad();
 }
 
 // Setting pseudo-randomized values for the truchet tiles demo page.
-function demoRandomize(){
+function demoRandomize() {
   // Setting demo tile style.
   let tilesRandStyle;
-  if (demoGetRandInt(1,100) > 15){ // 85% of the time show individual tile styles.
-    tilesRandStyle = String(demoGetRandInt(1,15));
-  }
-  else{ // 15% of the time show multiple tile styles.
-    let s = demoGetRandInt(5,15);
+  if (demoGetRandInt(1, 100) > 15) {
+    // 85% of the time show individual tile styles.
+    tilesRandStyle = String(demoGetRandInt(1, 15));
+  } else {
+    // 15% of the time show multiple tile styles.
+    let s = demoGetRandInt(5, 15);
     let lowerLimit = null;
     let upperLimit = null;
-    if(s >= 5 && s <= 6){
+    if (s >= 5 && s <= 6) {
       lowerLimit = 5;
       upperLimit = 6;
-    }
-    else if(s >= 7 && s <= 10){
+    } else if (s >= 7 && s <= 10) {
       lowerLimit = 7;
       upperLimit = 10;
-    }
-    else if(s >= 11 && s <= 15){
+    } else if (s >= 11 && s <= 15) {
       lowerLimit = 11;
       upperLimit = 15;
     }
-    newDemoStyle = String(s);
-    let sArr = [];
+    let newDemoStyle = String(s);
+    let styleArray = [];
     for (let i = lowerLimit; i <= upperLimit; i++) {
-      if(i != s){
-        sArr.push(i);
+      if (i != s) {
+        styleArray.push(i);
       }
     }
-    let randMultiple = demoGetRandInt(1,sArr.length);
+    let randMultiple = demoGetRandInt(1, styleArray.length);
     for (let i = 0; i < randMultiple; i++) {
-      let randIndex = demoGetRandInt(0,sArr.length-1);
-      newDemoStyle = newDemoStyle + '&' + String(sArr.splice(randIndex, 1)[0]);
+      let randIndex = demoGetRandInt(0, styleArray.length - 1);
+      newDemoStyle = newDemoStyle + "&" + String(styleArray.splice(randIndex, 1)[0]);
     }
     tilesRandStyle = newDemoStyle;
   }
-  // Setting demo tile colors
-  let tilesRandColor1 = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6,0);
-  let tilesRandColor2 = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6,0);
-  let tilesRandColor3 = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6,0);
-  // Setting demo tile outline thickness
+  // Setting demo tile colors.
+  let tilesRandColor1 =
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0);
+  let tilesRandColor2 =
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0);
+  let tilesRandColor3 =
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0);
+  // Setting demo tile outline thickness.
   let tilesRandThickness;
-  if (demoGetRandInt(1,100) > 15){ // 85% of the time pick a random outline thickness from 25-100.
-    tilesRandThickness = String(demoGetRandInt(25,100));
-  }
-  else{ // 15% of the time show no outline.
-    tilesRandThickness = '0';
+  if (demoGetRandInt(1, 100) > 15) {
+    // 85% of the time pick a random outline thickness from 25-100.
+    tilesRandThickness = String(demoGetRandInt(25, 100));
+  } else {
+    // 15% of the time show no outline.
+    tilesRandThickness = "0";
   }
   // Setting demo tile size.
   let tilesRandSize;
-  if (demoGetRandInt(1,100) > 15){ // 85% of the time pick a data size between 5 and 15.
-    tilesRandSize = String(demoGetRandInt(5,15));
-  }
-  else{ // 15% of the time pick a data size between 1-5 or 16-50.
-    let r_size = demoGetRandInt(1,40);
-    if (r_size >= 6 && r_size <= 15){
-      r_size = r_size + 10;
+  if (demoGetRandInt(1, 100) > 15) {
+    // 85% of the time pick a data size between 5 and 15.
+    tilesRandSize = String(demoGetRandInt(5, 15));
+  } else {
+    // 15% of the time pick a data size between 1-5 or 16-50.
+    let randSize = demoGetRandInt(1, 40);
+    if (randSize >= 6 && randSize <= 15) {
+      randSize = randSize + 10;
     }
-    tilesRandSize = String(r_size);
+    tilesRandSize = String(randSize);
   }
-  demoLoadAllTiles(tilesRandStyle,tilesRandColor1,tilesRandColor2,tilesRandColor3,tilesRandThickness,tilesRandSize);
-}
-
-// Overwrites Context Menu for touching image on mobile.
-function demoTilesPreventContextMenuMobile(event){
-  event.preventDefault();
-  return false;
-}
-
-// Handles selecting tiles on the demo page on mobile.
-document.getElementById('tiles-wrapper').addEventListener('touchstart', function (e) {
-  e.preventDefault() // Turns off onclick event.
-  if(e.touches.length === 1){
-    demoTileChange(e, e.touches[0].target, false);
-  }
-  else if(e.touches.length > 1 && e.touches[0].target.classList.contains('tile-selected')) {
-    console.log()
-    demoTileChange(e, e.touches[1].target, true);
-  }
-});
-
-// Handle selecting tiles on the demo page.
-function demoTileSelect(event, element){
-  demoTileChange(event,element,event.ctrlKey)
+  demoLoadAllTiles(
+    tilesRandStyle,
+    tilesRandColor1,
+    tilesRandColor2,
+    tilesRandColor3,
+    tilesRandThickness,
+    tilesRandSize
+  );
 }
 
 // Handle changing tiles on the demo page.
-function demoTileChange(event, element, multiple){
-  if(!multiple){ // Selecting one tile.
-    demoSetStyle(element.getAttribute('data-value'), true);
-  }
-  else{ // Selecting multiple tiles.
-    let elementStyle = element.getAttribute('data-value');
-    let selectedTiles = document.getElementsByClassName('tile-selected');
-    let selectedStyles = Array.from(selectedTiles).map(x => x.getAttribute('data-value'));
+function demoTileChange(element, multiple) {
+  if (!multiple) {
+    // Selecting one tile.
+    demoSetStyle(element.getAttribute("data-value"), true);
+  } else {
+    // Selecting multiple tiles.
+    let elementStyle = element.getAttribute("data-value");
+    let selectedTiles = document.getElementsByClassName("demo-grid-tile-selected");
+    let selectedStyles = Array.from(selectedTiles).map((x) => x.getAttribute("data-value"));
     let newTile = true;
-    let newStyle = '';
+    let newStyle = "";
     // Checking to see if selecting a tile that is already selected.
     for (let i = 0; i < selectedStyles.length; i++) {
-      if(elementStyle === selectedStyles[i]){
+      if (elementStyle === selectedStyles[i]) {
         newTile = false;
-        if(selectedStyles.length > 1){ // Remove tile already selected.
+        if (selectedStyles.length > 1) {
+          // Remove tile already selected.
           selectedStyles.splice(i, 1);
-          newStyle = selectedStyles.join('&');
+          newStyle = selectedStyles.join("&");
           demoSetStyle(newStyle, true);
         }
         break;
       }
     }
-    if (newTile){ // Adding new tile.
+    if (newTile) {
+      // Adding new tile.
       selectedStyles.push(elementStyle);
-      newStyle = selectedStyles.join('&');
+      newStyle = selectedStyles.join("&");
       demoSetStyle(newStyle, true);
     }
   }
 }
 
 // Overrides moving to the next input field and keeping keyboard open on Enter for color input fields.
-var elements = document.getElementsByClassName('color-input');
-for (var i = 0; i < elements.length; i++) {
-  elements[i].addEventListener('keyup', function (e) {
-    if (e.key === 'Enter') {
+let elements = document.getElementsByClassName("demo-grid-color-input");
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
       this.blur();
     }
   });
@@ -208,50 +223,61 @@ for (var i = 0; i < elements.length; i++) {
 function demoColorChange(element) {
   let styleTest = new Option().style;
   styleTest.color = element.value;
-  let isColor =  styleTest.color !== '';
-  if(isColor){ // Checks if input is valid color.
-    if(element.id == 'color-input-1'){
-      demoSetColor1(element.value, true);
+  let isColor = styleTest.color !== "";
+  if (isColor) {
+    // Checks if input is valid color.
+    if (element.id == "demo-grid-color-input-1") {
+      demoSetColor(element.value, 1, true);
+    } else if (element.id == "demo-grid-color-input-2") {
+      demoSetColor(element.value, 2, true);
+    } else if (element.id == "demo-grid-color-input-3") {
+      demoSetColor(element.value, 3, true);
     }
-    else if(element.id == 'color-input-2'){
-      demoSetColor2(element.value, true);
-    }
-    else if(element.id == 'color-input-3'){
-      demoSetColor3(element.value, true);
-    }
-  }
-  else{ // Show error color if input is invalid color.
-    element.style.borderColor = '#cc0000';
+  } else {
+    // Show error color if input is invalid color.
+    element.style.borderColor = "#cc0000";
   }
 }
 
 // Handles when a demo color input field is out of focus. Resets element value to the last valid demo color.
-function demoColorFocusOut(element){
-  element.value = element.getAttribute('data-value');
-  element.style.borderColor = '#292F33';
+function demoColorFocusOut(element) {
+  element.value = element.getAttribute("data-value");
+  element.style.borderColor = "#292F33";
 }
 
 // Handles changes in the slider fields.
 function demoSliderChange(element) {
-  if(element.id == 'outline-thickness-slider'){
+  if (element.id == "demo-grid-outline-thickness-slider") {
     demoSetThickness(element.value, true);
-  }
-  else if(element.id == 'size-slider'){
+  } else if (element.id == "demo-grid-size-slider") {
     demoSetSize(element.value, true);
   }
 }
 
 // Handles copying the canvas element to clipboard.
-function demoCanvasCopy(){
-  let canvasElement = document.getElementById('truchet-tiles');
-  let html_string = '<canvas id=\'truchet-tiles\' ' +
-                      'data-style=\'' + canvasElement.getAttribute('data-style') + '\' ' +
-                      'data-color-1=\'' + canvasElement.getAttribute('data-color-1') + '\' ' +
-                      'data-color-2=\'' + canvasElement.getAttribute('data-color-2') + '\' ' +
-                      'data-color-3=\'' + canvasElement.getAttribute('data-color-3') + '\' ' +
-                      'data-outline-thickness=\'' + canvasElement.getAttribute('data-outline-thickness') + '\' ' +
-                      'data-size=\'' + canvasElement.getAttribute('data-size') + '\' ' +
-                    '></canvas>';
+function demoCanvasCopy() {
+  let canvasElement = document.getElementById("truchet-tiles");
+  let html_string =
+    "<canvas id='truchet-tiles' " +
+    "data-style='" +
+    canvasElement.getAttribute("data-style") +
+    "' " +
+    "data-color-1='" +
+    canvasElement.getAttribute("data-color-1") +
+    "' " +
+    "data-color-2='" +
+    canvasElement.getAttribute("data-color-2") +
+    "' " +
+    "data-color-3='" +
+    canvasElement.getAttribute("data-color-3") +
+    "' " +
+    "data-outline-thickness='" +
+    canvasElement.getAttribute("data-outline-thickness") +
+    "' " +
+    "data-size='" +
+    canvasElement.getAttribute("data-size") +
+    "' " +
+    "></canvas>";
   navigator.clipboard.writeText(html_string);
 }
 
